ecd0037ae03f9125afce596bc1fe0e8661ae9e2b
Lukas Betancur <lbetancur@ucvts.tec.nj.us>
2019-10-28 09:17:14 -0400

    exercise 1

--- src/ProblemSet5.java
+++ src/ProblemSet5.java
@@ -19,7 +19,11 @@
 public class ProblemSet5 {
     
     public static void main(String[] args) {
-        
+        ProblemSet5 ps = new ProblemSet5();
+
+        System.out.println(ps.surroundMe("in","here"));
+
+
     }
     
     /*
@@ -30,7 +34,18 @@ public class ProblemSet5 {
      */
     
     public String surroundMe(String in, String out) {
-
+        String outcome;
+        
+         if(in != null && out != null && out.length() == 4){
+            String lastTwoOut = out.substring(out.length()-2);
+            String firstTwoOut = out.substring(0,2);
+            outcome = firstTwoOut + in + lastTwoOut;
+            return outcome;
+        } else {
+            return in;
+        }
+       
+        
     }
     
     /*
@@ -40,89 +55,108 @@ public class ProblemSet5 {
      * and last n characters of text.
      */
     
+     
     public String endsMeet(String text, int n) {
-
+            
     }
+
+    
     
     /*
      * Exercise 3.
      * 
      * Given a string, return a new string using the middle three characters of text.
-     */
+    
     
     public String middleMan(String text) {
-
+        
     }
+
+     */
     
     /*
      * Exercise 4.
      * 
      * Given two strings, determine whether or not target is equivalent to the middle
      * three characters of text.
-     */
+     
     
     public boolean isCentered(String text, String target) {
 
     }
     
+    */
+
     /*
      * Exercise 5.
      * 
      * Given a string and a character, compute the number of words that end in suffix.
-     */
+     
     
     public int countMe(String text, char suffix) {
 
     }
+
+    */
     
     /*
      * Exercise 6.
      * 
      * Given a string, compute the number of triplets in text.
-     */
+     
     
     public int triplets(String text) {
 
     }
+
+    */
     
     /*
      * Exercise 7.
      * 
      * Given a string, compute the sum of the digits in text.
-     */
+     
     
     public long addMe(String text) {
 
     }
+
+    */
     
     /*
      * Exercise 8.
      * 
      * Given a string, compute the length of the longest sequence.
-     */
+    
     
     public long sequence(String text) {
 
     }
+
+     */
     
     /*
      * Exercise 9.
      * 
      * Given two strings, return a new string built by intertwining each of the
      * characters of a and b.
-     */
+     
     
     public String intertwine(String a, String b) {
 
     }
+
+    */
     
     /*
      * Exercise 10.
      * 
      * Given a string, determine whether or not it is a palindrome.
-     */
+     
     
     public boolean isPalindrome(String text) {
 
     }
+
+    */
 }
